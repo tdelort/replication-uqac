36e77953b318f309f24b2641f049cd37da66eafa
tdelort <tristan.delort@gmail.com>
2021-10-28 18:11:51 -0400

    class registry

--- replication/include/ClassRegistry.h
+++ replication/include/ClassRegistry.h
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <functional>
+#include <map>
+
+#include "NetworkObject.h"
+
 namespace uqac::replication
 {
     class ClassRegistry
     {
     private:
         ClassRegistry();
+        std::map<uint32_t, std::function<NetworkObject(void)>> reg;
+
     public:
         ClassRegistry(const ClassRegistry&) = delete;
         ClassRegistry& operator=(const ClassRegistry&) = delete;
@@ -17,5 +24,10 @@ namespace uqac::replication
             static ClassRegistry classRegistry;
             return classRegistry;
         }
-    }
+
+        template<typename T>
+        static void Register<T>(std::function<NetworkObject(void)>);
+
+        static NetworkObject Create(uint32_t classID);
+    };
 }

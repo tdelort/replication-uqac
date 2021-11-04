75f91ac8f5a5130cdad5ae1c5344c54952de4327
tdelort <tristan.delort@gmail.com>
2021-11-04 18:13:30 -0400

    packet type

--- replication/include/Protocol.h
+++ replication/include/Protocol.h
@@ -1,7 +1,10 @@
-enum class PacketType : uint8_t
+namespace uqac::replication
 {
-    Hello = 0x00,
-    Sync = 0x01,
-    Bye = 0x02,
-    PacketType_Max
+    enum class PacketType : uint8_t
+    {
+        Hello = 0x00,
+        Sync = 0x01,
+        Bye = 0x02,
+        PacketType_Max
+    }
 }
